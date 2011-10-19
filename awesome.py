f0a895e0b880b18e6e8cc6c50fcfba6dd7eeeaef
Sebastian Torngren <basse.thorngren@gmail.com>
2011-10-19 10:55:29 +0200

    Final commit. Everything is finished.

--- awesome.py
+++ awesome.py
@@ -3,44 +3,63 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-import data
-data.init()
+import data, logging
 
+#Entering the awesome homepage
 @app.route("/")
 def home():
-    return render_template('index.html')
+    data.init()
+    log('Entering the awesome homepage!')
+    return render_template('start.html')
 
-@app.route("/search", methods = ['GET'])
-def search():
-    sokning = request.args.get('search')
-    sorting = request.args.get('sort')
-    cats = request.args.get('categories')
-    technique = request.args.get('technique')
-    by_sorting = request.args.get('by_sorting')
-    projects = data.retrieve_projects()[1]
-    if sokning != None:
-        sokning = sokning.encode('ascii')
-        sorting = sorting.encode('ascii')
-        cats = cats.encode('ascii')
-        technique = technique.encode('ascii')
-        by_sorting = by_sorting.encode('ascii')
-        if technique == 'Choose a technique':
-            technique = None
-        return render_template('search.html', proj_hit = data.retrieve_projects(search = sokning, search_fields = [cats], techniques = technique, sort_order = sorting, sort_by = by_sorting)[1])
-    else:
-        return render_template('search.html', projects = data.retrieve_projects()[1])
-
-@app.route("/list")
+#List all projects (sort by project name as standard) and search
+@app.route("/list", methods = ['GET'])
 def list():
-    return render_template('list.html', projects = data.retrieve_projects()[1])
+    data.init()
+    log('Searching for a project')
+    searchStr = request.args.get('search')
+    sorting = request.args.get('sort')
+    cats = request.args.getlist('categories')
+    by_sorting = request.args.get('by_sort')
+    technique = request.args.getlist('check_tech')
+
+    if searchStr != None:
+        searchStr = searchStr.encode('utf-8')
+    if by_sorting == None:
+        by_sorting = 'project_name'
+
+    proj_hit = data.retrieve_projects(search = searchStr, search_fields = cats, techniques = technique, sort_order = sorting, sort_by = by_sorting)[1]
+
+    if len(proj_hit) == 0:
+        return render_template('list.html', fail = 'No project found')
+
+    return render_template('list.html', proj_hit = data.retrieve_projects(search = searchStr, search_fields = cats, techniques = technique, sort_order = sorting, sort_by = by_sorting)[1])
 
 @app.route("/techniques")
-def techniques(tech = None):
-    return render_template('techniques.html', tech = data.retrieve_techniques()[1])
+def techniques():
+    data.init()
+    log('Displaying all techniques')
+    return render_template('techniques.html', techniques = data.retrieve_technique_stats()[1])
 
 @app.route("/project/<id>")
 def project(id):
-    return render_template('project.html', project = data.retrieve_projects(search=str(id), search_fields = ["project_no"])[1])
+    data.init()
+    id = id.encode('utf-8')
+    log('Displaying project ' + str(id))
+    project = data.retrieve_projects(search = str(id), search_fields = ['project_no'])[1]
+    if project == []:
+        return render_template('no_result.html')
+    
+    return render_template('project.html', project = data.retrieve_projects(search = str(id), search_fields = ['project_no'])[1])
+
+@app.errorhandler(404)
+def not_found(error):
+    log('404 error')
+    return render_template('page_not_found.html'), 404
+
+def log(logga):
+    logging.basicConfig(filename='awesome.log',level=logging.DEBUG)
+    logging.debug(logga)
 
 
 if __name__ == "__main__":
--- data.py
+++ data.py
@@ -7,21 +7,26 @@ list_techniques = []
 
 ############################ INIT()######################################
 def init():
-    #LÄS IN DATAFILEN
+    #READ IN THE CSV FILE
     global projects
+    global unicode_projects
     projects = csv.DictReader(open("data.csv"))   
+    unicode_projects = []
 
-    #GÖR OM TILL UNICODE OCH LÄGG IN TILL UNICODE_PROJECTS
+    #REDO THE VALUES TO UNICODE AND ADD THEM TO THE LIST UNICODE_PROJECTS
     for dicts in projects:
         temp_dict = {} 
         for value in dicts:
-            if value.find("no") != -1 and dicts[value].isdigit():
+            #IF PROJECT_NO IS A DIGIT, KEEP IT AS AN DIGIT (AKA NOT UNICODE)
+            if value.find("project_no") != -1 and dicts[value].isdigit():
                 temp_dict[unicode(value, "utf-8")] = int(dicts[value])
+            #IF VALUE IN DICT IS ANYTHING ELSE BUT A DIGIT, ENCODE TO UNICODE
             else:
                 temp_dict[unicode(value, "utf-8")] = unicode(dicts[value], "utf-8")
+        #ADD THE DICTIONARIES TO THE LIST UNICODE_PROJECTS
         unicode_projects.append(temp_dict)
     
-    #GÖR OM TECHNIQUES_USED TILL KOMMASEPARERADE LISTOR
+    #REDO TECHNIQUES_USED TO A COMMA-SEPARATED LIST
     for dicts in unicode_projects:
         temp = dicts['techniques_used'].split(',')
         list_techniques = temp
@@ -41,7 +46,7 @@ def init():
 ############################### PROJECT_COUNT() ##############################
 def project_count():   
 
-#KOLLA OM UNICODE_PROJECT ÄR TOM ELLER EJ
+#CHECK IF UNICODE_PROJECTS IS EMPTY OR NOT
     if len(unicode_projects) == 0:
         errcode = 1
         return (errcode, len(unicode_projects))
@@ -52,14 +57,15 @@ def project_count():
 ############################## LOOKUP_PROJECT(ID) ###########################
 def lookup_project(id):
 
-#KOLLA OM UNICODE_PROJECT ÄR TOM ELLER EJ
+#CHECK IF UNICODE_PROJECTS IS EMPTY OR NOT
     if len(unicode_projects) == 0:
-        print "LADDA INIT() FÖRST DUMBASS!"
-#OM MAN LETAR EFTER ETT ID STÖRRE ÄN VAD SOM FINNS
+        errcode = 1
+        return errcode, None
+#IF ID IS BIGGER THAN THE AMOUNT OF PROJECTS, RETURN AN ERROR
     elif id > len(unicode_projects):
         errcode = 2
         return errcode, None
-#RETURNERA DET PROJEKTET SOM ID:T ANGER
+#RETURN THE PROJECT
     else:
         for dicts in unicode_projects:
             if dicts['project_no'] == int(id):
@@ -68,12 +74,12 @@ def lookup_project(id):
 
 ############################# RETRIEVE_TECHNIQUES() ##########################
 def retrieve_techniques():
-#SKAPA EN LISTA DÄR ALLA TEKNIKER KOMMER HAMNA
+#MAKE A LIST WHERE ALL THE TECHNIQUES WILL GO
     dict_tec_lista = []
-#FÅ FRAM ALLA DICTIONARIES I LISTAN UNICODE_PROJECTS
+
     for dicts in unicode_projects:
         temp = dicts['techniques_used']
-#LETA FRAM ALLA UNIKA TEKNIKER OCH LÄGG I DE I DICT_TEC_LISTA
+#FIND ALL UNIQUE TECHNIQUES AND ADD THEM TO DICT_TEC_LISTA
         for i in temp:
             if i not in dict_tec_lista and len(i) > 0:
                 dict_tec_lista.append(i)
@@ -83,14 +89,14 @@ def retrieve_techniques():
 
 ############################# RETRIEVE_PROJECTS ################################
 def retrieve_projects(sort_by='start_date', sort_order='asc', techniques=None, search=None, search_fields=None):
-#SKAPA EN LISTA DÄR SÖKRESULTATEN KOMMER ATT HAMNA
+#MAKE A LIST WHERE THE SEARCHRESULTS WILL GO
     return_lista = []
 
-#FÅ FRAM ALLA DICTIONARIES I LISTAN UNICODE_PROJECTS    
+#GET ALL PROJECTS IN UNICODE_PROJECTS   
     for proj in unicode_projects:
         add = False
 
-#KOLLA OM MAN SKREV I SEARCH OCH SEARCH_FIELDS DELEN        
+#CHECK IF THE USER WROTE SOMETHING IN SEARCH OR SEARCH_FIELDS    
         if search or search_fields:
             for field in search_fields:
                 data = proj[field]
@@ -102,7 +108,7 @@ def retrieve_projects(sort_by='start_date', sort_order='asc', techniques=None, s
         else:
             add = True
 
-#KOLLA OM MAN SKREV I TECHNIQUES DELEN
+#CHECK IF THE USER WROTE IN TECHNIQUES_USED
         if add and techniques:
             for tech in techniques:
                 if not tech in proj['techniques_used']:
@@ -112,10 +118,10 @@ def retrieve_projects(sort_by='start_date', sort_order='asc', techniques=None, s
         if add:
             return_lista.append(proj)
 
-#KOLLA HUR MAN VILL SORTERA, (START_DATE ELLER END_DATE)
+#CHECK HOW THE USER WANT TO SORT
     return_lista.sort(key=lambda val: val[sort_by])
 
-#KOLLA SORT_ORDER, (DESC ELLER ASC)    
+#DESC OR ASC    
     if sort_order == 'desc':
         return_lista.reverse()
 
@@ -124,20 +130,19 @@ def retrieve_projects(sort_by='start_date', sort_order='asc', techniques=None, s
 
 ##################################### RETRIEVE_TECHNIQUE_STATS() ###################
 def retrieve_technique_stats():
-#FLERA VACKRA VARIABLER
+#A LOT OF BEAUTIFUL VARIABLES
     temp_dict = {}
     proj_dict = {}
-    temp_list = []
     asd_dict = {}
     dsa_list = []
 
-#KOLLA VILKA TEKNIKER SOM ANVÄNTS    
+#CHECK WHICH TECHNIQUES THAT WERE USED  
     for tech in retrieve_techniques()[1]:
         x = 0
         temp_list = []
         temp_dict = {'name': str(tech)}
 
-#KOLLA HUR MÅNGA GÅNGER TEKNIKENS HAR ANVÄNTS        
+#CHECK HOW MANY TIMES THAT TECHNIQUE WAS USED     
         for dicts in unicode_projects:
             if tech in dicts['techniques_used']:
                 x += 1
@@ -147,7 +152,7 @@ def retrieve_technique_stats():
                 asd_dict = {}
         temp_dict['count'] = x
 
-#SORTERA EFTER PROJEKTETS NAMN
+#SORT BY THE PROJECTS NAME
         temp_list = sorted(temp_list, key=lambda asd_dict: asd_dict['name'])
 
         temp_dict['projects'] = temp_list
